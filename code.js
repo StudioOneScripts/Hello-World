edeebb738b1ec1cba06127fd8eb1a839c4fa8f81
StudioOneScripts <StudioOneScripts@users.noreply.github.com>
2019-01-16 15:23:33 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,59 @@
+﻿
+const kPackageID = "Hello World Example";
+
+function myScript() 
+{
+    this.interfaces =
+    [
+        Host.Interfaces.IEditTask,
+        Host.Interfaces.IParamObserver,
+        Host.Interfaces.IController
+    ]
+
+    // ---------------------------------------------------------------------
+    
+    this.prepareEdit = function (context)
+    {        
+        // parameter list for gui objects
+        this.paramList = Host.Classes.createInstance("CCL:ParamList")
+        this.paramList.controller = this;
+
+        // add the button
+        this.button = this.paramList.addParam("MyButton");
+
+        // Show the gui form
+        Host.GUI.runDialog(Host.GUI.Themes.getTheme(kPackageID), "MyForm", this);
+        
+        return Host.Results.kResultOk; 
+    }
+
+    // ---------------------------------------------------------------------
+
+    this.performEdit = function (context)
+    {    
+        return Host.Results.kResultOk; 
+    }
+
+    // ---------------------------------------------------------------------
+
+    // Act on GUI objects
+    this.paramChanged = function (param)
+    {
+        //Host.GUI.alert(param.name)
+
+        // Clicked the button
+        if (param == this.button)
+        {
+            Host.GUI.alert("Hello World!");
+        } 
+        
+        return Host.Results.kResultOk; 
+    }
+}
+
+// ---------  ENTRY FUNCTION  ------------------------------------------
+
+function createInstance()
+{
+    return new myScript;
+}
